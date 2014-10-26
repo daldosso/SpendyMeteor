4b9a7ba2eb68adc0d3c32c3fcec0848467036ed1
Alberto Dal Dosso <daldosso@gmail.com>
2014-10-26 15:14:23 +0100

    A simple todo list

--- SpendyMeteor.js
+++ SpendyMeteor.js
@@ -1,23 +1,45 @@
-if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault("counter", 0);
+Items = new Meteor.Collection('items');
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get("counter");
+if (Meteor.isClient) {
+  Template.list.helpers({
+    items: function() {
+      return Items.find();  
+    },
+    doneClass: function() {
+      if (this.done) {
+        return 'done';
+      } else {
+        return '';
+      }
     }
   });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set("counter", Session.get("counter") + 1);
+  Template.controls.events({
+    'submit form': function(event) {
+      event.preventDefault();
+      var description = $(event.target).find('[id=newItem]').val();
+      Items.insert({ description: description });
+    }
+  });
+
+  Template.list.events({
+    'click li': function() {
+      Items.update({_id:this._id}, {$set:{done:!this.done}})
     }
   });
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
-    // code to run on server at startup
+    if (Items.find().count() == 0) {
+      var items = [{
+        description: 'Item 1'
+      }, {
+        description: 'Item 2'
+      }];
+      for (var i=0; i<items.length; i++) {
+        Items.insert(items[i]);
+      }
+    }
   });
 }
